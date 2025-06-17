59862f351f42d9e11d15691a3caac9be5728f8bd
Caio Kormives <caio.kormives@ti.hubfloripa.com.br>
2025-06-17 07:23:44 -0300

    Adiciona o campo 'description' ao modelo de tarefas e atualiza a migração correspondente para incluir a nova coluna na tabela 'tasks'.

--- lib/colecaomoda_back/tasks/task.ex
+++ lib/colecaomoda_back/tasks/task.ex
@@ -2,9 +2,10 @@ defmodule ColecaomodaBack.Tasks.Task do
   use Ecto.Schema
   import Ecto.Changeset
 
-  @derive {Jason.Encoder, only: [:id, :title, :completed, :inserted_at, :updated_at]}
+  @derive {Jason.Encoder, only: [:id, :title, :description, :completed, :inserted_at, :updated_at]}
   schema "tasks" do
     field :title, :string
+    field :description, :string
     field :completed, :boolean, default: false
 
     timestamps(type: :utc_datetime)
@@ -13,7 +14,7 @@ defmodule ColecaomodaBack.Tasks.Task do
   @doc false
   def changeset(task, attrs) do
     task
-    |> cast(attrs, [:title, :completed])
+    |> cast(attrs, [:title, :description, :completed])
     |> validate_required([:title])
   end
 end
---
+++ priv/repo/migrations/20250617102050_add_description_to_tasks.exs
@@ -0,0 +1,9 @@
+defmodule ColecaomodaBack.Repo.Migrations.AddDescriptionToTasks do
+  use Ecto.Migration
+
+  def change do
+    alter table(:tasks) do
+      add :description, :text
+    end
+  end
+end
